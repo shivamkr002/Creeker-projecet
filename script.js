604ae138968f682b35bf84051481a9021c507f4e
shivam kumar <shivamkr2587@gmail.com>
2024-10-21 17:22:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggle() {
+  document.getElementById("Mobile-menu").classList.add("show");
+}
+function toggleout() {
+  document.getElementById("Mobile-menu").classList.remove("show");
+}
